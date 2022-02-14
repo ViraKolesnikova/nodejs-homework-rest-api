486431a9dec74dc05d1e49cd3ac576ae8f12f679
ViraKolesnikova <biralara@ukr.net>
2022-02-14 09:56:14 +0200

    get, getById, post methods

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,48 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const path = require("path");
 
-const listContacts = async () => {}
+const {v4} = require('uuid')
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+  try {
+    const contacts = await fs.readFile(path.join(__dirname, "contacts.json"), "utf-8");
+    return JSON.parse(contacts);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-const removeContact = async (contactId) => {}
+const getContactById = async (contactId) => {
+  try {
+    const contacts = await listContacts();
+    const [contactById] = contacts.filter((item) => item.id === contactId);
+    return contactById;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-const addContact = async (body) => {}
+const removeContact = async (contactId) => {};
 
-const updateContact = async (contactId, body) => {}
+const addContact = async (body) => {
+  try{
+    const contacts = await listContacts();
+    const {name, email, phone} = body;
+    const newContact = {
+      id: v4(),
+        name,
+        email,
+        phone
+    };
+    contacts.push(newContact);
+    await fs.writeFile(path.join(__dirname,'..','models', 'contacts.json'), JSON.stringify(contacts));
+    return newContact;
+  }catch(error){
+    console.log(error);
+  }
+};
+
+const updateContact = async (contactId, body) => {};
 
 module.exports = {
   listContacts,
@@ -16,4 +50,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,74 @@
-const express = require('express')
+const express = require("express");
 
-const router = express.Router()
+const Joi = require('joi');
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+  updateContact,
+} = require("../../models/contacts");
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const router = express.Router();
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (req, res, next) => {
+  const contacts = await listContacts();
+  if (!contacts) {
+    res.status(400).json({
+      status: "Error 400",
+      message: `Bad request`,
+    });
+    return
+  }
+  res.json({ status: "success", data: contacts });
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  const id = req.params.contactId;
+  const contactById = await getContactById(id);
+  if (!contactById) {
+    res.status(404).json({
+      status: "Error 404",
+      message: `Contact with id ${id} is not found`,
+    });
+    return
+  }
+  res.json({ status: "success", data: contactById });
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res, next) => {
+  const schema = Joi.object({
+    name: Joi.string()
+      .alphanum()
+      .min(3)
+      .max(30)
+      .required(),
+    email: Joi.string()
+      .email({ minDomainSegments: 2 })
+      .required(),
+    phone: Joi.string()
+      .pattern(/^[0-9 ]+$/, 'numbers')
+      .min(10)
+      .max(16)
+      .required()
+  })
 
-module.exports = router
+  const validationResult = schema.validate(req.body);
+  if(validationResult.error){
+    res.status(400).json({"message": validationResult.error.details});
+  }
+     
+ const newContact = await addContact(req.body);
+  res.status(201).json({ status: "Created", data: newContact });
+});
+
+router.delete("/:contactId", async (req, res, next) => {
+  res.json({ message: "delete contact" });
+});
+
+router.put("/:contactId", async (req, res, next) => {
+  res.json({ message: "change contact" });
+});
+
+module.exports = router;
